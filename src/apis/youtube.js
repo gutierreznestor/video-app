3d0ea4b55674a5ff70c141e93fff84b49b7dede6
GutierrezNestor <nggutierrez.45@gmail.com>
2020-05-28 12:15:23 -0300

    Youtube configuration

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
-export const KEY = 'AIzaSyAfBQJHl3ZM2pmvETH5wSzX6vT3tkgl_gE';
+import axios from 'axios';
+
+const KEY = 'AIzaSyAfBQJHl3ZM2pmvETH5wSzX6vT3tkgl_gE';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
