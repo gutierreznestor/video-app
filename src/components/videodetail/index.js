aac4ef288dd7d6feccae6043fbe44f87d1fdb084
GutierrezNestor <nggutierrez.45@gmail.com>
2020-05-28 15:07:35 -0300

    Show video detail with iframe

--- src/components/videodetail/index.js
+++ src/components/videodetail/index.js
@@ -3,10 +3,24 @@ import { Container, Title } from './VideoDetail';
 
 const VideoDetail = ({ video }) => {
   if (!video) return <div>Loading...</div>;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <Container>
-      <Title>{video.snippet.title}</Title>
-      <p>{video.snippet.description}</p>
+      <div>
+        <iframe
+          title={video.snippet.title}
+          width='560'
+          height='315'
+          src={videoSrc}
+          frameborder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div>
+        <Title>{video.snippet.title}</Title>
+        <p>{video.snippet.description}</p>
+      </div>
     </Container>
   );
 };
